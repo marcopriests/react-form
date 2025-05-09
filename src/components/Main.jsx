3ffa5250a20cf9ce126a4dd1d6e4e86e2f6e8ce4
marcopriests <prtmrc98@gmail.com>
2025-05-09 18:21:37 +0200

    [UPD] Esercizio base completato

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -4,20 +4,32 @@ import { useState } from 'react'
 const articoli = ['Articolo 1', 'Articolo 2', 'Articolo 3', 'Articolo 4', 'Articolo 5', 'Articolo 6',]
 
 const Main = () => {
-    const {articolo, setArticolo} = useState('')
+    const [articolo, setArticolo] = useState('')
+    const [articoliIniziali, setArticoliIniziali] = useState(articoli)
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        setArticoliIniziali([...articoliIniziali, articolo])
+        setArticolo('')
+    }
 
     return (
         <div>
             <div className="container">
                 <h1 className="title">Lista Articoli</h1>
                 <ul className="list-group">
-                    {articoli.map((art) => {
-                        return <li className="list-item">{art}</li>
+                    {articoliIniziali.map((art, i) => {
+                        return <li className="list-item" key={i}>{art}</li>
                     })}
                 </ul>
 
-                <form action="">
-                    <input type="text" placeholder='Aggiungi articolo' value={articolo} onChange={e => {setArticolo(e.target.value)}} />
+                <form onSubmit={handleSubmit}>
+                    <input
+                        type="text"
+                        placeholder='Aggiungi articolo'
+                        value={articolo}
+                        onChange={e => { setArticolo(e.target.value) }} />
+
                     <button className="btn">Aggiungi</button>
                 </form>
             </div>
