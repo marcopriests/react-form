83df353818d5cc1945a2be15153ceddcef4f9e03
marcopriests <prtmrc98@gmail.com>
2025-05-09 18:05:20 +0200

    [UPD] onChange input fatto

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import { useState } from 'react'
 
 const articoli = ['Articolo 1', 'Articolo 2', 'Articolo 3', 'Articolo 4', 'Articolo 5', 'Articolo 6',]
 
 const Main = () => {
+    const {articolo, setArticolo} = useState('')
+
     return (
         <div>
             <div className="container">
@@ -12,6 +15,11 @@ const Main = () => {
                         return <li className="list-item">{art}</li>
                     })}
                 </ul>
+
+                <form action="">
+                    <input type="text" placeholder='Aggiungi articolo' value={articolo} onChange={e => {setArticolo(e.target.value)}} />
+                    <button className="btn">Aggiungi</button>
+                </form>
             </div>
         </div>
     )
